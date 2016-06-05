af5f3622e6a3b0a05b283ac15ba4f38d8d9f0e7d
Ignacio Giagante <igiagante@gmail.com>
2016-06-05 19:09:39 -0300

    start working on show plant screen

---
+++ app/src/main/java/com/example/igiagante/thegarden/showPlant/presentation/ShowPlantFragment.java
@@ -0,0 +1,11 @@
+package com.example.igiagante.thegarden.showPlant.presentation;
+
+import com.example.igiagante.thegarden.core.presentation.BaseFragment;
+
+/**
+ * @author Ignacio Giagante, on 6/5/16.
+ */
+public class ShowPlantFragment extends BaseFragment {
+
+
+}
